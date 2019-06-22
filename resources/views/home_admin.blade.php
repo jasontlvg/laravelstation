bdfa7c35490f959b6c5e8bf3212820c82aa4075a
shieldvg <shieldvg@gmail.com>
2019-06-22 02:37:20 -0700

    agregamos components - saber si iniciamos sesion como user o admin, o tambien los dos

--- resources/views/home_admin.blade.php
+++ resources/views/home_admin.blade.php
@@ -7,14 +7,9 @@
             <div class="card">
                 <div class="card-header">Admin Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                <div class="p-3">
+                    @component('components.who')
+                    @endcomponent
                 </div>
             </div>
         </div>
